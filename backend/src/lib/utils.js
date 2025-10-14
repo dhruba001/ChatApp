3679bfd866f5450c3017306b0c571ae27cbcd549
Dhruba <gdhruba748+github@gmail.com>
2025-10-14 18:19:27 +0530

    added comments

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,9 +2,9 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "7d", // user will have to login after 7 days
   });
-
+  // generating token and then sending to user in cookie
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // maximum age in milliseconds
     httpOnly: true,
