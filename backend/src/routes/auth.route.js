37c7f53584a2de08338d59786255cf49a7dc6b55
Dhruba <gdhruba748+github@gmail.com>
2025-10-14 23:02:45 +0530

    new routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,6 +4,7 @@ import {
   login,
   logout,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -18,5 +19,7 @@ router.post("/logout", logout);
 // update profile will only run if user is authenticated,
 //so that is the task of middleware protectRoute : auth.middleware.js
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth); // to get auth status from protectroute middleware
 
 export default router;
