5a3ea2357d14c81f42a6d8b2a819d46886cd2aae
Dhruba <gdhruba748+github@gmail.com>
2025-10-14 16:49:24 +0530

    index changed for dotenv and db connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 
-app.listen(5001, () => {
-  console.log("server is running on port 5001");
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("server is running on localhost://" + PORT);
+  connectDB();
 });
