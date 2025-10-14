18eb451f9d363902e83af22f28bfebd3766d26f3
Dhruba <gdhruba748+github@gmail.com>
2025-10-14 21:07:50 +0530

    logout api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,5 +89,13 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout Successfull " });
+  } catch (error) {
+    console.log("error in logout controller", error.message);
+    res
+      .status(500)
+      .json({ message: "internal server error while logging out" });
+  }
 };
