4cddd127fc5edb81236bdaddd573f5b24606c085
Dhruba <gdhruba748+github@gmail.com>
2025-10-15 08:58:44 +0530

    adding message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; // for getting jwt from cookie
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -13,7 +14,22 @@ const PORT = process.env.PORT;
 app.use(express.json()); // now in anywhere we can get data from req.body
 app.use(cookieParser());
 
+/*
+app.use("/api/auth", authRoutes); sets a base path for all routes defined inside authRoutes.
+
+So for example, if inside auth.route.js we have:
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+Then the actual endpoints become: POST /api/auth/signup , POST /api/auth/login
+
+Express automatically appends the paths from the router to the base path defined in app.use().
+*/
+
+//api routing paths : authroutes is auth.route.js
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on localhost://" + PORT);
