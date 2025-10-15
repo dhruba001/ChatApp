4a38f5e97cd0596009973785de1d77dc1fe1d32e
Dhruba <gdhruba748+github@gmail.com>
2025-10-15 10:27:21 +0530

    getMessage api

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+} from "../controllers/message.controller.js";
 
 const router = express();
 
 //api's will be declared here
 
 router.get("/users", protectRoute, getUsersForSidebar);
+
+//* get messages for the users i am chatting with, so if say i chatted with 2 users
+//* they will come as 2 disting chats, so clicking on each will fetch the chats for that users
+//* only, and it will come when we send the user id
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
