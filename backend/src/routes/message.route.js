a5492b6ec049159e5d2755252a86c493ddea0f6c
Dhruba <gdhruba748+github@gmail.com>
2025-10-15 11:24:36 +0530

    sendMessage route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getUsersForSidebar,
   getMessages,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express();
@@ -15,5 +16,11 @@ router.get("/users", protectRoute, getUsersForSidebar);
 //* they will come as 2 disting chats, so clicking on each will fetch the chats for that users
 //* only, and it will come when we send the user id
 router.get("/:id", protectRoute, getMessages);
+
+//* here this api is for sending message between authenticated users
+//* message might be text or image
+//* this api will be used by both sender, like me and the guy i am chatting with
+//* both will use other one's id to send message test or image to one another
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
