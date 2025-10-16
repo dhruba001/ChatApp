5de7c912d6862fe04d9f24c15e1a5bfea39dcf47
Dhruba <gdhruba748+github@gmail.com>
2025-10-16 22:33:32 +0530

    changed json return data

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -8,7 +8,7 @@ export const getUsersForSidebar = async (req, res) => {
       id: { $ne: loggedInUserId },
     }).select("-password"); // find loggedin users except the one currently logged in
     // denoted by $ne, so if the db has 4 users the crrent user will see other 3 excluding him/her
-    res.status(200).json({ message: "users for sidebar" }, filteredUsers);
+    res.status(200).json(filteredUsers);
     //sending the list of users back to client
   } catch (error) {
     console.log("error in getting getUsersForSidebar", error.message);
